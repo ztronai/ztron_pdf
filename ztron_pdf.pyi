866bac74b6456b7d1ffccb7b4cffce0e54b91405
Mahit Mehta <mahit.py@gmail.com>
2025-09-10 17:20:45 -0400

    pyi hotfix

--- ztron_pdf.pyi
+++ ztron_pdf.pyi
@@ -1,8 +1,8 @@
 from typing import List
 
 class PyPageData:
-    base64_image: str
+    image_buffer: bytes
 
-def render_base64_pdf(base64_pdf: str, format: str, quality: int, max_edge_size: int) -> List[PyPageData]: ...
+def render_base64_pdf(pdf_bytes: bytes, quality: int) -> List[PyPageData]: ...
 
 def compress_pdf(base64_pdf: str, quality: int) -> str: ...
